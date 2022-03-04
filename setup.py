a5feb607d2b3d52227034b3b4fb1998fb40c7f38
John R <JohnREngineer@Gmail.com>
2022-03-04 11:00:09 -0800

    Module (#3)
    * Updated file structure.
    
    * Renamed file.
    
    * Renamed files.
    
    * Updated module files.
    
    * Added default behavior to main.
    
    * Changed file configuration
    
    * Delete .vscode directory
    
    * Remove colab functions
    
    * Update readme
    
    * Make pip installable.
    
    * Update package name.

---
+++ setup.py
@@ -0,0 +1,22 @@
+
+import setuptools
+
+with open("README.md", "r", encoding="utf-8") as fh:
+    long_description = fh.read()
+
+setuptools.setup(
+    name='drive_etl_tools',
+    version='0.0.3',
+    author='Mike Huls',
+    author_email='JohnREngineer@gmail.com',
+    description='Tools for doing automated ETL for sheets and files in Google Drive.',
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url='https://github.com/JohnREngineer/drive_etl_tools',
+    project_urls = {
+        "Bug Tracker": "https://github.com/mike-huls/JohnREngineer/drive_etl_tools"
+    },
+    license='https://www.fsf.org/licensing/licenses/agpl-3.0.html',
+    packages=['drive_etl_tools'],
+    install_requires=['pydrive2','gspread','pandas'],
+)
